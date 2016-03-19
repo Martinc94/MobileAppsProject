6c8afe44d9ebae81eed94d1e5b22b8f7401b19c2
Martin Coleman <Martinc94@live.co.uk>
2016-03-19 19:20:14 +0000

    Added Data Binding To GaaResults page

--- Match Tracker/GaaResults.xaml.cs
+++ Match Tracker/GaaResults.xaml.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows.Input;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Storage;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -20,29 +22,35 @@ namespace Match_Tracker
     {
         public List<listContent> ContentList { get; set; }
 
-
-        //test
-       // String str="0";
+        public List<Result> ResultsList;
 
         public GaaResults()
         {
             this.InitializeComponent();
             loadResults();
-            //fillListView();
-            
-            
         }
 
-        private void addToListView(String str){
+        public void addToListView(String str){
             
             listContent listItem = new listContent();
-
             listItem.Result = str;
+            //listItem.Tap += TextBlock_Tapped;   
+            
 
             ResultsListView.Items.Add(listItem);
             ResultsListView.DataContext = ContentList;
-
         }
+
+        public void addResultToListView(Result re)
+        {
+
+            
+            ResultsListView.Items.Add(re);
+            //ResultsListView.DataContext = ContentList;
+            ResultsListView.DataContext = ResultsList;
+        }
+
+
 
         private async void loadResults()
         {
@@ -51,8 +59,7 @@ namespace Match_Tracker
             StorageFile sampleFile;
             try
             {
-                //open gaa folder
-                //
+                //get results
                 sampleFile = await storageFolder.GetFileAsync("gaaResults.txt");
             }
             catch (Exception myE)
@@ -61,58 +68,93 @@ namespace Match_Tracker
                 return;
             }
             string fileText = await Windows.Storage.FileIO.ReadTextAsync(sampleFile);
-            
-
-            //test
-            //string str = fileText;
 
             parseFile(fileText);
-
-            //addToListView(str);
-
-            
-
-            //Windows.UI.Popups.MessageDialog m = new Windows.UI.Popups.MessageDialog(str);
-
-            //read line 
-            //parse line 
-            //add to list
-            //output list of results
         }
 
         private void parseFile(String fileText)
         {
-            //string[] res = fileText.Split('\n');
-            string[] res = fileText.Split('\n');
-          //  addToListView(res[0]);
-           // addToListView(res[1]);
-           // addToListView(res[2]);
+            //splits up all results
+            string[] res = fileText.Split('\n');       
 
-            for (int i= 0;i<res.Length;i++) {
+            for (int i= 0;i<res.Length-1;i++) {
                 String str= res[i];
-                addToListView(res[i]);
+                //addToListView(res[i]);
+                //add to result
+                Result r = new Result(str);
+                String ResStr=r.getFormattedResult();
+
+                //addToListView(ResStr);
+
+                addResultToListView(r);
             }
+        }
+
+        private void OnItemTapped(object sender, TappedEventHandler e)
+        {
+            /* if (e.Item != null && this.TappedEventHandler != null && this.TappedEventHandler.CanExecute(e))
+             {
+                 this.ItemClickCommand.Execute(e.Item);
+                 this.SelectedItem = null;
+             }*/
+
+           // MessageDialog m = new MessageDialog("Tapped");
         }
 
         public class listContent
         {
-            public string Result { get; set; }
-            
+            public string Result { get; set; }  
         }
 
-        public class result
+       
+
+       
+
+        private static void OnItemClick(object sender, ItemClickEventArgs e)
         {
-            public string Result { get; set; }
-            public string TeamOneName { get; set; }
-            public string TeamTwoName { get; set; }
-
-            //add tapped event
-
-            //split by :
-            /* for (int i = 0; i<res.Length;i++) {
-                String str = res[i];
-                addToListView(res[i]);
-            }*/
+            MessageDialog m = new MessageDialog("Tapped");
+            
+        }
     }
+
+    public class Result
+    {
+        public string result { get; set; }
+        public string TeamOneName;
+        public string TeamOneGoal;
+        public string TeamOnePoint;
+        public string TeamTwoName;
+        public string TeamTwoGoal;
+        public string TeamTwoPoint;
+
+        public Result()
+        { 
+
+        }
+
+        public Result(String ResultString)
+        {
+            //splits results
+            String[] res = ResultString.Split(':');
+            this.TeamOneName = res[0];
+            this.TeamOneGoal = res[1];
+            this.TeamOnePoint = res[2];
+            this.TeamTwoName = res[3];
+            this.TeamTwoGoal = res[4];
+            this.TeamTwoPoint = res[5];
+
+            this.result = getFormattedResult();
+        }
+
+        public String getFormattedResult()
+        {
+            String result = TeamOneName + " : " + TeamOneGoal + "-" + TeamOnePoint + " - " + TeamTwoName + " : " + TeamTwoGoal + "-" + TeamTwoPoint;
+            return result;
+        }
+
+        //add tapped event
+
     }
+
+
 }
